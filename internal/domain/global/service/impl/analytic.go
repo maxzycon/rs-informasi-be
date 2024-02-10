f8c6c10bc8293baf34be7f5d2272ae03ef74fe08
max <maxzycon@gmail.com>
2024-02-10 15:59:45 +0800

    [hotfix] split analytic dashboard

--- internal/domain/global/service/impl/analytic.go
+++ internal/domain/global/service/impl/analytic.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/maxzycon/rs-farmasi-be/internal/domain/global/dto"
+	"github.com/maxzycon/rs-farmasi-be/pkg/authutil"
 	typequeue "github.com/maxzycon/rs-farmasi-be/pkg/constant/type_queue"
 )
 
@@ -57,11 +58,29 @@ func (s *GlobalService) GetDashboardAnalytic(ctx context.Context) (resp *dto.Sum
 		},
 	}
 
+	user, _ := authutil.GetCredential(ctx)
+
+	if user.MerchantID == nil {
+		err = s.Summary(resp)
+		if err != nil {
+			return
+		}
+		return
+	}
+
+	err = s.SummaryByMerchantId(resp, *user.MerchantID)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (s *GlobalService) SummaryByMerchantId(dto *dto.SummaryDashboardWrapper, id uint) (err error) {
 	yearlyChart, err := s.db.
 		Raw(`SELECT COUNT(id) as total, MONTH(created_at) as bulan FROM queues
-				WHERE YEAR(created_at) = YEAR(now())
-				AND deleted_at IS NULL
-				GROUP BY MONTH(created_at)`).
+		WHERE YEAR(created_at) = YEAR(now())
+		AND deleted_at IS NULL AND merchant_id = ?
+		GROUP BY MONTH(created_at)`, id).
 		Rows()
 
 	if err != nil {
@@ -78,15 +97,15 @@ func (s *GlobalService) GetDashboardAnalytic(ctx context.Context) (resp *dto.Sum
 			return
 		}
 		temp.Label = bulanMap[temp.Label]
-		resp.QueuesChart = append(resp.QueuesChart, temp)
+		dto.QueuesChart = append(dto.QueuesChart, temp)
 	}
 	// ----- Location
 	locationChart, err := s.db.
 		Raw(`SELECT COUNT(q.id) as total, l.name 
-				FROM queues q
-				LEFT JOIN locations l ON l.id = q.location_id
-				WHERE YEAR(q.created_at) = YEAR(now()) AND MONTH(q.created_at) = MONTH(now())
-				GROUP BY location_id`).
+		FROM queues q
+		LEFT JOIN locations l ON l.id = q.location_id
+		WHERE YEAR(q.created_at) = YEAR(now()) AND MONTH(q.created_at) = MONTH(now()) AND merchant_id = ? AND q.deleted_at IS NULL 
+		GROUP BY location_id`, id).
 		Rows()
 	if err != nil {
 		return
@@ -100,13 +119,13 @@ func (s *GlobalService) GetDashboardAnalytic(ctx context.Context) (resp *dto.Sum
 		if err != nil {
 			return
 		}
-		resp.QueuesLocationChart = append(resp.QueuesLocationChart, temp)
+		dto.QueuesLocationChart = append(dto.QueuesLocationChart, temp)
 	}
 	// ----- Type
 	typeChart, err := s.db.Raw(`SELECT COUNT(id) as total, type FROM queues
-				WHERE YEAR(created_at) = YEAR(now()) AND MONTH(created_at) = MONTH(now())
-				AND deleted_at IS NULL
-				GROUP BY type`).
+		WHERE YEAR(created_at) = YEAR(now()) AND MONTH(created_at) = MONTH(now()) AND merchant_id = ?
+		AND deleted_at IS NULL
+		GROUP BY type`, id).
 		Rows()
 	if err != nil {
 		return
@@ -123,20 +142,21 @@ func (s *GlobalService) GetDashboardAnalytic(ctx context.Context) (resp *dto.Sum
 		}
 
 		if temp.Type == typequeue.RACIKAN {
-			resp.QueuesType.Racikan = temp.Value
+			dto.QueuesType.Racikan = temp.Value
 		}
 
 		if temp.Type == typequeue.NON_RACIKAN {
-			resp.QueuesType.NonRacikan = temp.Value
+			dto.QueuesType.NonRacikan = temp.Value
 		}
 	}
 	// ----- avg by type (difference in minutes)
 	avgType, err := s.db.Raw(`SELECT AVG(COALESCE(TIMESTAMPDIFF(MINUTE, p.created_at, d.created_at), 0)) AS avg_difference, q.type
-				FROM queues q
-				LEFT JOIN (SELECT queue_id, status FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id))  as l ON l.queue_id = q.id
-				JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 2))  as p ON p.queue_id = q.id 
-				JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 3))  as d ON d.queue_id = q.id 
-				GROUP BY q.type`).Rows()
+		FROM queues q
+		LEFT JOIN (SELECT queue_id, status FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id))  as l ON l.queue_id = q.id
+		JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 2))  as p ON p.queue_id = q.id 
+		JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 3))  as d ON d.queue_id = q.id 
+		WHERE q.merchant_id = ? AND q.deleted_at IS NULL 
+		GROUP BY q.type`, id).Rows()
 
 	if err != nil {
 		return
@@ -154,30 +174,161 @@ func (s *GlobalService) GetDashboardAnalytic(ctx context.Context) (resp *dto.Sum
 		}
 
 		if temp.Type == typequeue.RACIKAN {
-			resp.QueuesAvgType.Racikan = temp.Value
+			dto.QueuesAvgType.Racikan = temp.Value
 		}
 
 		if temp.Type == typequeue.NON_RACIKAN {
-			resp.QueuesAvgType.NonRacikan = temp.Value
+			dto.QueuesAvgType.NonRacikan = temp.Value
 		}
 	}
 
 	// ----- avg by status
 	err = s.db.Raw(`SELECT 
-			AVG(COALESCE(TIMESTAMPDIFF(MINUTE, f.created_at, t.created_at), 0)) as validation_to_process,
-			AVG(COALESCE(TIMESTAMPDIFF(MINUTE, t.created_at, th.created_at), 0)) as process_to_ready_to_be_submitted,
-			AVG(COALESCE(TIMESTAMPDIFF(MINUTE, th.created_at, fr.created_at), 0)) as ready_to_be_submitted_to_submitted
-			FROM queues q
-			LEFT JOIN (SELECT queue_id, status FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id))  as l ON l.queue_id = q.id
-			JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 1))  as f ON f.queue_id = q.id 
-			JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 2))  as t ON t.queue_id = q.id 
-			JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 3))  as th ON th.queue_id = q.id 
-			JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 4))  as fr ON fr.queue_id = q.id `).
+	COALESCE(AVG(TIMESTAMPDIFF(MINUTE, f.created_at, t.created_at)), 0) as validation_to_process,
+	COALESCE(AVG(TIMESTAMPDIFF(MINUTE, t.created_at, th.created_at)), 0) as process_to_ready_to_be_submitted,
+	COALESCE(AVG(TIMESTAMPDIFF(MINUTE, th.created_at, fr.created_at)), 0) as ready_to_be_submitted_to_submitted
+	FROM queues q
+	LEFT JOIN (SELECT queue_id, status FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id))  as l ON l.queue_id = q.id
+	JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 1))  as f ON f.queue_id = q.id 
+	JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 2))  as t ON t.queue_id = q.id 
+	JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 3))  as th ON th.queue_id = q.id 
+	JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 4))  as fr ON fr.queue_id = q.id 
+	WHERE q.merchant_id = ? AND q.deleted_at IS NULL`, id).
 		Row().
-		Scan(&resp.QueuesAvgStatus.ValidationToProcess, &resp.QueuesAvgStatus.ProcessToReadyToBeSubmitted, &resp.QueuesAvgStatus.ReadyToBeSubmitted)
+		Scan(&dto.QueuesAvgStatus.ValidationToProcess, &dto.QueuesAvgStatus.ProcessToReadyToBeSubmitted, &dto.QueuesAvgStatus.ReadyToBeSubmitted)
 
 	if err != nil {
 		return
 	}
+
+	return
+}
+
+func (s *GlobalService) Summary(dto *dto.SummaryDashboardWrapper) (err error) {
+	yearlyChart, err := s.db.
+		Raw(`SELECT COUNT(id) as total, MONTH(created_at) as bulan FROM queues
+		WHERE YEAR(created_at) = YEAR(now())
+		AND deleted_at IS NULL
+		GROUP BY MONTH(created_at)`).
+		Rows()
+
+	if err != nil {
+		return
+	}
+
+	for yearlyChart.Next() {
+		temp := &struct {
+			Label string "json:\"label\""
+			Value uint64 "json:\"value\""
+		}{}
+		err = yearlyChart.Scan(&temp.Value, &temp.Label)
+		if err != nil {
+			return
+		}
+		temp.Label = bulanMap[temp.Label]
+		dto.QueuesChart = append(dto.QueuesChart, temp)
+	}
+	// ----- Location
+	locationChart, err := s.db.
+		Raw(`SELECT COUNT(q.id) as total, l.name 
+		FROM queues q
+		LEFT JOIN locations l ON l.id = q.location_id
+		WHERE YEAR(q.created_at) = YEAR(now()) AND MONTH(q.created_at) = MONTH(now()) AND q.deleted_at IS NULL 
+		GROUP BY location_id`).
+		Rows()
+	if err != nil {
+		return
+	}
+	for locationChart.Next() {
+		temp := &struct {
+			Label string "json:\"label\""
+			Value uint64 "json:\"value\""
+		}{}
+		err = locationChart.Scan(&temp.Value, &temp.Label)
+		if err != nil {
+			return
+		}
+		dto.QueuesLocationChart = append(dto.QueuesLocationChart, temp)
+	}
+	// ----- Type
+	typeChart, err := s.db.Raw(`SELECT COUNT(id) as total, type FROM queues
+		WHERE YEAR(created_at) = YEAR(now()) AND MONTH(created_at) = MONTH(now())
+		AND deleted_at IS NULL
+		GROUP BY type`).
+		Rows()
+	if err != nil {
+		return
+	}
+	for typeChart.Next() {
+		temp := &struct {
+			Type  uint
+			Value uint64
+		}{}
+		err = typeChart.Scan(&temp.Value, &temp.Type)
+
+		if err != nil {
+			return
+		}
+
+		if temp.Type == typequeue.RACIKAN {
+			dto.QueuesType.Racikan = temp.Value
+		}
+
+		if temp.Type == typequeue.NON_RACIKAN {
+			dto.QueuesType.NonRacikan = temp.Value
+		}
+	}
+	// ----- avg by type (difference in minutes)
+	avgType, err := s.db.Raw(`SELECT AVG(COALESCE(TIMESTAMPDIFF(MINUTE, p.created_at, d.created_at), 0)) AS avg_difference, q.type
+		FROM queues q
+		LEFT JOIN (SELECT queue_id, status FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id))  as l ON l.queue_id = q.id
+		JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 2))  as p ON p.queue_id = q.id 
+		JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 3))  as d ON d.queue_id = q.id 
+		WHERE q.deleted_at IS NULL 
+		GROUP BY q.type`).Rows()
+
+	if err != nil {
+		return
+	}
+
+	for avgType.Next() {
+		temp := &struct {
+			Type  uint
+			Value float64
+		}{}
+		err = avgType.Scan(&temp.Value, &temp.Type)
+
+		if err != nil {
+			return
+		}
+
+		if temp.Type == typequeue.RACIKAN {
+			dto.QueuesAvgType.Racikan = temp.Value
+		}
+
+		if temp.Type == typequeue.NON_RACIKAN {
+			dto.QueuesAvgType.NonRacikan = temp.Value
+		}
+	}
+
+	// ----- avg by status
+	err = s.db.Raw(`SELECT 
+	COALESCE(AVG(TIMESTAMPDIFF(MINUTE, f.created_at, t.created_at)), 0) as validation_to_process,
+	COALESCE(AVG(TIMESTAMPDIFF(MINUTE, t.created_at, th.created_at)), 0) as process_to_ready_to_be_submitted,
+	COALESCE(AVG(TIMESTAMPDIFF(MINUTE, th.created_at, fr.created_at)), 0) as ready_to_be_submitted_to_submitted
+	FROM queues q
+	LEFT JOIN (SELECT queue_id, status FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id))  as l ON l.queue_id = q.id
+	JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 1))  as f ON f.queue_id = q.id 
+	JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 2))  as t ON t.queue_id = q.id 
+	JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 3))  as th ON th.queue_id = q.id 
+	JOIN (SELECT queue_id, status, id, created_at FROM queue_histories as c WHERE id = (SELECT MAX(qh.id) FROM queue_histories as qh WHERE qh.queue_id = c.queue_id and qh.status = 4))  as fr ON fr.queue_id = q.id
+	WHERE q.deleted_at IS NULL `).
+		Row().
+		Scan(&dto.QueuesAvgStatus.ValidationToProcess, &dto.QueuesAvgStatus.ProcessToReadyToBeSubmitted, &dto.QueuesAvgStatus.ReadyToBeSubmitted)
+
+	if err != nil {
+		return
+	}
+
 	return
 }
