5a580faff1a22499612060dea4f8c51d946c4d93
max <maxzycon@gmail.com>
2024-03-01 20:21:12 +0800

    [hotfix] adding merchant id str

--- pkg/authutil/authutil.go
+++ pkg/authutil/authutil.go
@@ -5,17 +5,19 @@ import (
 
 	"github.com/gofiber/fiber/v2/log"
 	"github.com/maxzycon/rs-farmasi-be/pkg/errors"
+	uuid "github.com/satori/go.uuid"
 )
 
 type UserClaims struct {
-	ID         uint    `json:"id"`
-	Username   string  `json:"username"`
-	Name       string  `json:"name"`
-	Email      string  `json:"email"`
-	Phone      *string `json:"phone"`
-	Photo      *string `json:"photo"`
-	MerchantID *uint   `json:"merchant_id"`
-	Role       uint    `json:"role"`
+	ID            uint       `json:"id"`
+	Username      string     `json:"username"`
+	Name          string     `json:"name"`
+	Email         string     `json:"email"`
+	Phone         *string    `json:"phone"`
+	Photo         *string    `json:"photo"`
+	MerchantID    *uint      `json:"merchant_id"`
+	MerchantIDStr *uuid.UUID `json:"merchant_id_str"`
+	Role          uint       `json:"role"`
 }
 
 func GetCredential(f context.Context) (resp *UserClaims, err error) {
